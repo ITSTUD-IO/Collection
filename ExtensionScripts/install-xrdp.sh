1137263d1bed797d9528539fdce217235e6f19ae
Christian Mueller <christian.mueller@milc.de>
2020-07-20 17:52:52 +0200

    added chrome

--- ExtensionScripts/install-xrdp.sh
+++ ExtensionScripts/install-xrdp.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 sudo apt update
-sudo apt install --Yes xfce4 xfce4-goodies xorg dbus-x11 x11-xserver-utils
-sudo apt install --Yes xrdp
+sudo apt install --yes xfce4 xfce4-goodies xorg dbus-x11 x11-xserver-utils
+sudo apt install --yes xrdp
 sudo adduser xrdp ssl-cert
 sudo ufw allow 3389
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt install --yes ./google-chrome-stable_current_amd64.deb
