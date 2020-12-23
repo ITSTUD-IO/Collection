8766ee26da0fa211ba44e7bc88229a163fedc268
Christian Mueller <christian.mueller@milc.de>
2020-12-23 22:27:49 +0100

    update

--- AutomationScripts/BackupKeyVaultSecrets.ps1
+++ AutomationScripts/BackupKeyVaultSecrets.ps1
@@ -1,19 +1,15 @@
-Function BackupKeyVaultSecrets(
-    [string]$subscriptionId,
-    [string]$keyVaultName,
-    [string]$storageConnectionString,
-    [string]$fileShareName
-)
+$subscriptionId = Get-AzureAutomationVariable -Name SubscriptionId
+$storageConnectionString = Get-AzureAutomationVariable -Name BackupStorageConnectionString
+$keyVaultName = Get-AzureAutomationVariable -Name KeyVaultName
+$fileShareName = Get-AzureAutomationVariable -Name FileShareName
+$timestamp = Get-Date
+$directoryname = "$($timestamp.Year)$($timestamp.Month)$($timestamp.Day)-$($timestamp.Hour)$($timestamp.Minute)$($timestamp.Second)"
+az account set --subscription $subscriptionId
+az storage directory create --name $directoryname --share-name $fileShareName --connection-string $storageConnectionString
+$keyVaultEntries = (az keyvault secret list --vault-name $keyVaultName | ConvertFrom-Json) | Select-Object id, name
+foreach($entry in $keyVaultEntries)
 {
-    $timestamp = Get-Date
-    $directoryname = "$($timestamp.Year)$($timestamp.Month)$($timestamp.Day)-$($timestamp.Hour)$($timestamp.Minute)$($timestamp.Second)"
-    az account set --subscription $subscriptionId
-    az storage directory create --name $directoryname --share-name $fileShareName --connection-string $storageConnectionString
-    $keyVaultEntries = (az keyvault secret list --vault-name $keyVaultName | ConvertFrom-Json) | Select-Object id, name
-    foreach($entry in $keyVaultEntries)
-    {
-        az keyvault secret backup --file $entry.name --name $entry.name --vault-name $keyVaultName --subscription $subscriptionId
-        az storage file upload --share-name $fileShareName --path $directoryname --source $entry.name --connection-string $storageConnectionString
-        Remove-Item $entry.name
-    }
+    az keyvault secret backup --file $entry.name --name $entry.name --vault-name $keyVaultName --subscription $subscriptionId
+    az storage file upload --share-name $fileShareName --path $directoryname --source $entry.name --connection-string $storageConnectionString
+    Remove-Item $entry.name
 }
