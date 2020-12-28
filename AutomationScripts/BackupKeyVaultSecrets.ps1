e3ba129226ec45ead1d2f93ca5529312e19f12ec
Christian Mueller <christian.mueller@milc.de>
2020-12-28 10:56:31 +0100

    modified script

--- AutomationScripts/BackupKeyVaultSecrets.ps1
+++ AutomationScripts/BackupKeyVaultSecrets.ps1
@@ -1,15 +1,1 @@
-$subscriptionId = Get-AzureAutomationVariable -Name subscription_id
-$storageConnectionString = Get-AzureAutomationVariable -Name backup_storage_connection_string
-$keyVaultName = Get-AzureAutomationVariable -Name key_vault_name
-$fileShareName = Get-AzureAutomationVariable -Name file_share_name
-$timestamp = Get-Date
-$directoryname = "$($timestamp.Year)$($timestamp.Month)$($timestamp.Day)-$($timestamp.Hour)$($timestamp.Minute)$($timestamp.Second)"
-az account set --subscription $subscriptionId
-az storage directory create --name $directoryname --share-name $fileShareName --connection-string $storageConnectionString
-$keyVaultEntries = (az keyvault secret list --vault-name $keyVaultName | ConvertFrom-Json) | Select-Object id, name
-foreach($entry in $keyVaultEntries)
-{
-    az keyvault secret backup --file $entry.name --name $entry.name --vault-name $keyVaultName --subscription $subscriptionId
-    az storage file upload --share-name $fileShareName --path $directoryname --source $entry.name --connection-string $storageConnectionString
-    Remove-Item $entry.name
-}
+#backup azure key vault
